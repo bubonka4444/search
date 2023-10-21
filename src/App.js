cd0da76795ca10d5a2e96c84304d38a6a287a4d8
bubonka4444 <buneev.18.03@gmail.com>
2023-10-21 13:22:49 +0400

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [searchStarted, setSearchStarted] = useState(false)
+  
+  //запрос, вводимый пользователем
+  let [userRequest, setUserRequest] = useState('')
+
+  let handleUserRequestInput = (e) => {
+    setUserRequest(e.target.value)
+  }
+
+  //поиск совпадений в заголовках
+  function findEquals (request = '') {
+    request = request.toLowerCase();
+    let topResult = [];
+    for (let item of data) {
+      let resultTitle = item.title.toLowerCase();
+      if (resultTitle.includes(request) &&
+      isCorrectLength(request)) {
+        topResult.push(item)
+      } else continue
+    }
+    return topResult;
+  }
+  
+  //поиск аббревиатур
+  function findAbbreviation (request = '') {
+    request = request.toLowerCase();
+    let abbreviations = [];
+    for (let item of data) {
+      let title = item.title.toLowerCase()
+      if (isCorrectLength(request)) {
+        let ArrForTitle = title.split(' ')
+          let titleAbbreviation = makeAbbreviation(ArrForTitle)
+          //сравнение введённой аббревиатуры с заголовком
+          if (titleAbbreviation.slice(0, request.length) == request) {
+            abbreviations.push(item)
+          } else continue
+        }
+    }
+    return abbreviations;
+  }
+
+  //аббревиатура из заголовка
+  function makeAbbreviation (arr) {
+    let abbreviation = arr.reduce((prevVal, curWord) => 
+    prevVal + (curWord ? curWord[0]: ''), '')
+    return abbreviation;
+  }
+
+  //поиск начинается, если пользователь ввёл минимум два символа
+  function isCorrectLength (str) {
+    return (str.length >= 2) ? true : false;
+  }
+  
+  let abbreviations = findAbbreviation(userRequest)
+  let topResult = findEquals(userRequest);
+  let AbbrTitle = abbreviations[0]?.category;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input  className="searchBar" type="text"  placeholder="Поиск..."
+      onChange={() => searchStarted ? setSearchStarted(searchStarted) : setSearchStarted(!searchStarted)}
+      onBlur={() => setSearchStarted(!searchStarted)}
+      value={userRequest}
+      onInput={handleUserRequestInput} 
+      />
+      
+      <section className={(searchStarted &&
+        abbreviations.length > 0 || 
+        topResult.length > 0) ? 'active' : 'not-active'}>
+        <ul className='abbr'>
+          
+          <p className='abbr-title'>{AbbrTitle}</p>
+
+          {abbreviations?.map((result, index) => (
+            <li key={index}>{result.title}</li>
+          ))}
+        </ul>
+        <ul className='equals'>
+          <p className='categories-title'>Рубрики</p>
+          {topResult?.map((result, index) => (
+            <li key={index}>
+              <p>{result.category}</p>
+              {result.title}
+              </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
+  
 }
 
 export default App;
